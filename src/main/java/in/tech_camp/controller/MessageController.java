184f87ba21d544b3c2147ca242bd465dc02495dd
ryokikudo_ss <ryoki.kudo@smart-group.co.jp>
2026-05-08 15:57:01 +0900

    フロントの実装、viewの実装

---
+++ src/main/java/in/tech_camp/controller/MessageController.java
@@ -0,0 +1,14 @@
+package in.tech_camp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MessageController {
+  @GetMapping("/")
+  public String showMessages() {
+      return "messages/index";
+  }
+  
+}
