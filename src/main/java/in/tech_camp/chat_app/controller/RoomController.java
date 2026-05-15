3976f72ef8a39118f1eca8adc4ef93c864689107
ryokikudo_ss <ryoki.kudo@smart-group.co.jp>
2026-05-15 11:31:04 +0900

    roomsテーブルの作成

---
+++ src/main/java/in/tech_camp/chat_app/controller/RoomController.java
@@ -0,0 +1,17 @@
+package in.tech_camp.chat_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import in.tech_camp.chat_app.form.RoomForm;
+
+@Controller
+public class RoomController {
+  
+  @GetMapping("/rooms/new")
+  public String showRoomForm(Model model) {
+    model.addAttribute("roomForm", new RoomForm());
+    return "rooms/new";
+  }
+}
